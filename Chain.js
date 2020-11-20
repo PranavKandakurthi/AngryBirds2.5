6e688a4c7dc5408b7cb31e7b7991a8351f42cde6
PranavKandakurthi <kandakurthipranav@gmail.com>
2020-11-19 19:45:43 -0500

    Chain class created

---
+++ Chain.js
@@ -0,0 +1,29 @@
+class Chain{
+
+constructor(body1,body2){
+ var options = {
+bodyA: body1,
+bodyB: body2,
+stiffness: 0.1,
+length: 10
+ }
+
+ this.chain = Constraint.create(options);
+ World.add(world,this.chain);
+
+}
+
+display(){
+    var pointA = this.chain.bodyA.position;
+    var pointB = this.chain.bodyB.position;
+
+
+    stroke("brown");
+    strokeWeight(6);
+    line(pointA.x, pointA.y, pointB.x, pointB.y);
+
+}
+
+
+
+}
